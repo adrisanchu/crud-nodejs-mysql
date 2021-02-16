7af557e16ccbad0acaf28205bb33756f8deb9100
adrisanchu <adrian.sanchez@ree.es>
2021-02-16 09:42:43 +0100

    setup morgan to track requests made to the server

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 
 // settings
 // to change when we move to REE server
@@ -9,6 +9,12 @@ app.set('port', process.env.PORT || 3000);
 // set ejs as html template
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// middlewares
+// (executes before user requests)
+app.use(morgan('dev'));
+
+// routes
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
