147cb237b45cb7dc26e3452ba01ab22fd0a2aa70
adrisanchu <adrian.sanchez@ree.es>
2021-02-16 09:38:45 +0100

    setup ejs as HTML template engine

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const app = express();
 // settings
 // to change when we move to REE server
 app.set('port', process.env.PORT || 3000);
+// set ejs as html template
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
