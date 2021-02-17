10dcffd23327238d84bb0f488d0e7f303bde3a62
adrisanchu <adrian.sanchez@ree.es>
2021-02-17 09:13:34 +0100

    MVC - controller: call .ejs file from customerController

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -2,7 +2,18 @@ const controller = {};
 
 // add methods to the controller object
 controller.list = (req, res) => {
-    res.send('hello world');
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM customer', (err, customers) => {
+            if (err) {
+                res.json(err);
+            }
+            console.log('customers');
+            console.log(customers);
+            res.render('customers', {
+                data: customers
+            });
+        });
+    });
 };
 
 // controller.save
