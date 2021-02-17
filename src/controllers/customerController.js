508185b83364fbeac8a2bdc609550de5e4f1b2f1
adrisanchu <adrian.sanchez@ree.es>
2021-02-17 09:03:43 +0100

    MVC - controller: set a controller for customer view

---
+++ src/controllers/customerController.js
@@ -0,0 +1,11 @@
+const controller = {};
+
+// add methods to the controller object
+controller.list = (req, res) => {
+    res.send('hello world');
+};
+
+// controller.save
+// controller.delete
+
+module.exports = controller;
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const customerController = require('../controllers/customerController');
+
 // routes of our project to be exported ...
+router.get('/', customerController.list);
+
 
 module.exports = router;
