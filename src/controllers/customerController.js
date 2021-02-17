51f9bbc104943783f124c04ca510e015834770c4
adrisanchu <adrian.sanchez@ree.es>
2021-02-17 20:32:27 +0100

    MVC - controller: redirect to main page after submit data

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -20,8 +20,7 @@ controller.save = (req, res) => {
     const data = req.body;
     req.getConnection((err, conn) => {
         conn.query('INSERT INTO customer SET ?', [data], (err, customer) => {
-            console.log(customer);
-            res.send('it worked!');
+            res.redirect('/');
         });
     });
 };
